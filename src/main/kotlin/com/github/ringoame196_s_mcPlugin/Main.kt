b54095ed71e6ea0f536069d8a0025ad8503725a0
りんご飴196 <kazumac41@gmail.com>
2024-10-31 20:58:14 +0900

    🎉  :初期設定

---
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
@@ -0,0 +1,12 @@
+package com.github.ringoame196_s_mcPlugin
+
+import com.github.ringoame196_s_mcPlugin.events.PlayerInteractEvent
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+    override fun onEnable() {
+        super.onEnable()
+        val plugin = this
+        server.pluginManager.registerEvents(PlayerInteractEvent(), plugin)
+    }
+}
---
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/events/PlayerInteractEvent.kt
@@ -0,0 +1,11 @@
+package com.github.ringoame196_s_mcPlugin.events
+
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerInteractEvent
+
+class PlayerInteractEvent : Listener {
+    @EventHandler
+    fun onPlayerInteract(e: PlayerInteractEvent) {
+    }
+}
