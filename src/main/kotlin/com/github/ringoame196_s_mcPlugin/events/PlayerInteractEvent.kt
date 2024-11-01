18f590a727f7e9b02f38c0266139d8bb65c695b1
りんご飴196 <kazumac41@gmail.com>
2024-11-01 21:26:22 +0900

    ✨  :座標をGUI側に保存するように

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/events/PlayerInteractEvent.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/events/PlayerInteractEvent.kt
@@ -12,17 +12,16 @@ class PlayerInteractEvent : Listener {
 
     @EventHandler
     fun onPlayerInteract(e: PlayerInteractEvent) {
-        val block = e.clickedBlock ?: return
         val player = e.player
+        val location = player.location.block.location
         val item = e.item ?: return
 
-        if (!block.type.toString().contains("SAPLING")) return
         if (player.gameMode != GameMode.CREATIVE) return
         if (!player.isSneaking) return
         if (item.type != Material.BONE_MEAL) return
 
         e.isCancelled = true
-        val gui = treeGUIManager.makeGUi()
+        val gui = treeGUIManager.makeGUi(location)
         player.openInventory(gui)
     }
 }
